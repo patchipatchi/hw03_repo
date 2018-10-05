9b631b759e3073a3ade2acc318296981bdc87c4d
memory_server_side <memory_server_side@vultr.guest>
2018-10-05 02:59:01 +0000

    server stuff

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 export MIX_ENV=prod
-export PORT=4791
+export PORT=4792
 
 echo "Stopping old copy of app, if any..."
 
-/home/hw03/memory/_build/prod/rel/memory/bin/memory stop || true
+/home/memory_server_side/hw03_repo/_build/prod/rel/memory/bin/memory stop || true
 
 echo "Starting app..."
 
 # TODO: You want start.
 
-/home/hw03/memory/_build/prod/rel/memory/bin/memory start
+/home/memory_server_side/hw03_repo/_build/prod/rel/memory/bin/memory start
 
 # TODO: Add a cron rule or systemd service file
 #       to start your app on system boot.
