a7dc97ec9696035ea448134bd515a2536351cad4
hw05 <hw05@vultr.guest>
2018-10-11 23:18:45 +0000

    server start script change to new user

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ export PORT=4793
 
 echo "Stopping old copy of app, if any..."
 
-/home/memory_server_side/hw03_repo/_build/prod/rel/memory/bin/memory stop || true
+/home/hw05/hw03_repo/_build/prod/rel/memory/bin/memory stop || true
 
 echo "Starting app..."
 
 # TODO: You want start.
 
-/home/memory_server_side/hw03_repo/_build/prod/rel/memory/bin/memory start
+/home/hw05/hw03_repo/_build/prod/rel/memory/bin/memory start
 
 # TODO: Add a cron rule or systemd service file
 #       to start your app on system boot.
